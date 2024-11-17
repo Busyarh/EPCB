aa795b7b5fb92d382740c13be4a904ccb39e7e5a
Adewoyin Busyarh <adewoyinbolaji29@gmail.com>
2024-11-17 18:43:41 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,592 @@
+const questions = [
+
+// English starts 
+    {
+        question: "His joy knew no __________when he heard he had passed the interview",
+        answers: [
+            { text: "boundary", correct: false},
+            { text: "bounds", correct: true},
+            { text: "bound", correct: false},
+            { text: "bond", correct: false},
+        ]
+    },
+    
+    {
+        question: "The oranges were shared ___________Laide, Hussein and_______",
+        answers: [
+            { text: "between/me", correct: false},
+            { text: "among/me", correct: true},
+            { text: "between/I", correct: false},
+            { text: "among/I", correct: false},
+        ]
+    },
+
+    {
+        question: "King Saul was ___________on Mount Gilboa",
+        answers: [
+            { text: "Slayed", correct: false},
+            { text: "Slew", correct: false},
+            { text: "Slaid", correct: false},
+            { text: "Slain", correct: true},
+        ]
+    },
+
+    {
+        question: "After too much exercise, some people suffer from cramp which is caused by the ___________of the muscles",
+        answers: [
+            { text: "contraction", correct: true},
+            { text: "Restriction", correct: false},
+            { text: "conscription", correct: false},
+            { text: "Relaxation", correct: false},
+        ]
+    },
+
+    {
+        question: "If you travel by air you will be given an allowance to cover___________",
+        answers: [
+            { text: "baggages", correct: false},
+            { text: "a luggage", correct: false},
+            { text: "luggage", correct: true},
+            { text: "a baggage", correct: false},
+        ]
+    },
+
+    {
+        question: "Which strong determination Adamu was able to _________the problem",
+        answers: [
+            { text: "Surmount", correct: true},
+            { text: "Surpass", correct: false},
+            { text: "Defend", correct: false},
+            { text: "Know", correct: false},
+        ]
+    },
+
+    {
+        question: "It has been confirmed that the election ___________held in July",
+        answers: [
+            { text: "will be", correct: true},
+            { text: "is being", correct: false},
+            { text: "has been", correct: false},
+            { text: "have been", correct: false},
+        ]
+    },
+
+    {
+        question: "The girl says she is averse __________what others admire",
+        answers: [
+            { text: "to", correct: true},
+            { text: "with", correct: false},
+            { text: "for", correct: false},
+            { text: "from", correct: false},
+        ]
+    },
+
+    {
+        question: "The Emir and conqueror of the enemy territories____________ next week",
+        answers: [
+            { text: "arrives", correct: true},
+            { text: "are to arrive", correct: false},
+            { text: "arrive", correct: false},
+            { text: "are arriving", correct: false},
+        ]
+    },
+ 
+    {
+        question: "He said we shouldn't do it_________________?",
+        answers: [
+            { text: "Should we", correct: true},
+            { text: "Should be", correct: false},
+            { text: "didn't we", correct: false},
+            { text: "Said he", correct: false},
+        ]
+    },
+    
+    {
+        question: "The students had a ________on independence day",
+        answers: [
+            { text: "march pass", correct: false},
+            { text: "match pass", correct: false},
+            { text: "march past", correct: true},
+            { text: "match pass", correct: false},
+        ]
+    },
+    
+    {
+        question: "The attitude of my students to _________baffles me ",
+        answers: [
+            { text: "Pronunciation", correct: true},
+            { text: "Pronounciation", correct: false},
+            { text: "Pronunceation", correct: false},
+            { text: "Pronounceation", correct: false},
+        ]
+    },
+    
+    {
+        question: "Identify the word that has a different stress pattern",
+        answers: [
+            { text: "Competent", correct: false},
+            { text: "Represent", correct: true},
+            { text: "Syllabus", correct: false},
+            { text: "Quality", correct: false},
+        ]
+    },
+
+    {
+        question: "Identify the word that has a different stress pattern",
+        answers: [
+            { text: "Ability", correct: false},
+            { text: "Registration", correct: true},
+            { text: "Quadruplicate", correct: false},
+            { text: "Revivalist", correct: false},
+        ]
+    },
+
+    {
+        question: "Identify the word that has a different stress pattern",
+        answers: [
+            { text: "Canoe", correct: false},
+            { text: "knowledge", correct: true},
+            { text: "Impound", correct: false},
+            { text: "Subject(Verb)", correct: false},
+        ]
+    },
+
+    {
+        question: "The Life Changer: What is Ummi's occupation",
+        answers: [
+            { text: "Nurse", correct: false},
+            { text: "Teacher", correct: true},
+            { text: "Business Person", correct: false},
+            { text: "Accountant", correct: false},
+        ]
+    },
+
+    {
+        question: "The Life Changer:'Just call yourself Omar Esquire' this statement was made by who",
+        answers: [
+            { text: "Bint", correct: false},
+            { text: "Teemah", correct: true},
+            { text: "Jamila", correct: false},
+            { text: "Ummi", correct: false},
+        ]
+    },
+
+    {
+        question: "The Life Changer:'Money moves Mountains' this statement was made by who",
+        answers: [
+            { text: "Teemah", correct: false},
+            { text: "Salma", correct: true},
+            { text: "Bint", correct: false},
+            { text: "Ngozi", correct: false},
+        ]
+    },
+    
+    {
+        question: "Opposite in meaning:All attempts to bring him back from exile was FUTILE",
+        answers: [
+            { text: "Successful", correct: true},
+            { text: "Abortive", correct: false},
+            { text: "Fruitless", correct: false},
+            { text: "Impeccable", correct: false},
+        ]
+    },
+
+    {
+        question: "Opposite in meaning:His books was one of those books that GALVANIZED democracy",
+        answers: [
+            { text: "stimulated", correct: false},
+            { text: "encouraged", correct: false},
+            { text: "dissuaded", correct: true},
+            { text: "prompted", correct: false},
+        ]
+    },
+    // English ends 
+
+
+    // chemistry starts 
+    {
+        question: "Whose experiment showed that the atom has a tiny positively charged nucleus?",
+        answers: [
+            { text: "Thomson", correct: false},
+            { text: "Rutherford", correct: true},
+            { text: "Millikan", correct: false},
+            { text: "Dalton", correct: false},
+        ]
+    },
+    
+    {
+        question: "0.06g of a hydrocarbon occupies 32cm3 at STP. Its fomula is",
+        answers: [
+            { text: "C3H6", correct: true},
+            { text: "C2H2", correct: false},
+            { text: "C2H4", correct: false},
+            { text: "C2H6", correct: false},
+        ]
+    },
+    
+    {
+        question: "The functional group represented in the compound CH3-CH2-CH2-CH2-CH=O is",
+        answers: [
+            { text: "Alkanone", correct: false},
+            { text: "Alkanal", correct: true},
+            { text: "Alkanol", correct: false},
+            { text: "Alkanoate", correct: false},
+        ]
+    },
+    
+    {
+        question: "What mass of bromine will saturate completely 6.8g of 3-methylbut-1-ene [H=1, C=12, Br=80]",
+        answers: [
+            { text: "16g", correct: true},
+            { text: "32g", correct: false},
+            { text: "12g", correct: false},
+            { text: "24g", correct: false},
+        ]
+    },
+    
+    {
+        question: "The property used in the industrial preparation of Nitrogen and Oxygen from air is",
+        answers: [
+            { text: "Rate of diffusion", correct: false},
+            { text: "Density", correct: false},
+            { text: "boiling point", correct: true},
+            { text: "solubility", correct: false},
+        ]
+    },
+    
+    {
+        question: "Which of the following Quantum numbers divides shells into Orbitals",
+        answers: [
+            { text: "Principal", correct: false},
+            { text: "Subsidiary", correct: true},
+            { text: "Magnetic", correct: false},
+            { text: "Spin", correct: false},
+        ]
+    },
+
+    {
+        question: "What volume of 0.750moldm-3 Na2CO3 solution could be diluted to 250cm3 to reduce the concentration to 0.025moldm-3",
+        answers: [
+            { text: "16.8cm3", correct: false},
+            { text: "14.2cm3", correct: false},
+            { text: "10.4cm3", correct: false},
+            { text: "8.3cm3", correct: true},
+        ]
+    },
+
+    {
+        question: "The ability of an atom in a molecule to attract shared electrons to itself is",
+        answers: [
+            { text: "Electron Affinity", correct: true},
+            { text: "Ionic radius", correct: false},
+            { text: "ionization", correct: false},
+            { text: "Electronegativity", correct: false},
+        ]
+    },
+
+    {
+        question: "The factor that lowers the activation energy of a chemical reaction is",
+        answers: [
+            { text: "Pressure", correct: false},
+            { text: "Catalyst", correct: true},
+            { text: "Temperature", correct: false},
+            { text: "Concentration", correct: false},
+        ]
+    },
+
+    {
+        question: "500cm3 of Oxygen diffuse through a porous partition in 20secs. How long would it take 200cm3 of Carbondioxide to diffuse through it",
+        answers: [
+            { text: "50secs", correct: false},
+            { text: "9.4secs", correct: true},
+            { text: "80secs", correct: false},
+            { text: "40secs", correct: false},
+        ]
+    },
+    // chemistry ends 
+    // physics starts 
+
+    {
+        question: "The terminals of a battery of emf 24v and internal resistance of 1ohm is connected to an external resistor of 5ohms. Find the terminal p.d",
+        answers: [
+            { text: "18v", correct: false},
+            { text: "12v", correct: false},
+            { text: "16v", correct: false},
+            { text: "20v", correct: true},
+        ]
+    },
+
+    {
+        question: "A lorry accelerates uniformly in a straight line with acceleration of 4m/s2 and covers a distance of 250m in a time interval of 10s. How far will it travel in the next 10s?",
+        answers: [
+            { text: "650", correct: true},
+            { text: "900", correct: false},
+            { text: "800", correct: false},
+            { text: "250", correct: false},
+        ]
+    },
+
+    {
+        question: "The half life of a radioactive material is 12days. Calculate the decay constant",
+        answers: [
+            { text: "0.8663/day", correct: false},
+            { text: "0.04331/day", correct: false},
+            { text: "0.17325/day", correct: false},
+            { text: "0.05775/day", correct: true},
+        ]
+    },
+
+    {
+        question: "Which process is responsible for production of energy in stars",
+        answers: [
+            { text: "Nuclear reaction", correct: false},
+            { text: "Nuclear fission", correct: false},
+            { text: "Nuclear fusion", correct: true},
+            { text: "Radioactive decay", correct: false},
+        ]
+    },
+
+    {
+        question: "How much net work is required to accelerate a 1200kg car from 10m/s to 15m/s",
+        answers: [
+            { text: "1.95x10^5J", correct: false},
+            { text: "1.35x10^4J", correct: false},
+            { text: "7.5x10^4J", correct: true},
+            { text: "6.0x10^4J", correct: false},
+        ]
+    },
+
+    {
+        question: "A travelling wave of amplitude 0.80m has a frequency of 16Hz and a wave speed of 20m/s. Calculate the wave number of the wave",
+        answers: [
+            { text: "3", correct: false},
+            { text: "4", correct: false},
+            { text: "5", correct: true},
+            { text: "2", correct: false},
+        ]
+    },
+
+        {
+        question: "The pinhole camera works on",
+        answers: [
+            { text: "the principal of the reversibility of light", correct: false},
+            { text: "the principal of the rectilinear propagation of light", correct: true},
+            { text: "Huygen's principle", correct: false},
+            { text: "the principle of total internal reflection of light", correct: false},
+        ]
+    },
+
+    {
+        question: "A step down transformer is used on a2.2KV line to deliver 110v. How many turns are on the primary windlings if the secondary has 25turns",
+        answers: [
+            { text: "20", correct: false},
+            { text: "250", correct: false},
+            { text: "200", correct: false},
+            { text: "500", correct: true},
+        ]
+    },
+
+    {
+        question: "The branch of physics that deals with the motion of objects and the forces action on them is called",
+        answers: [
+            { text: "Electromagnetism", correct: false},
+            { text: "Thermodynamics", correct: false},
+            { text: "Mechanics", correct: true},
+            { text: "Quantum mechanics", correct: false},
+        ]
+    },
+
+    {
+        question: "The working of the beam balance is based on the principle of",
+        answers: [
+            { text: "moments", correct: true},
+            { text: "conservation of energy", correct: false},
+            { text: "equivalence", correct: false},
+            { text: "floatation", correct: false},
+        ]
+    },
+    // physics ends 
+    // biology starts 
+    {
+        question: "The movement of blood between the heart and all other parts of the body beside the lungs is",
+        answers: [
+            { text: "Double circulatory system", correct: false},
+            { text: "Pulmonary system", correct: false},
+            { text: "Single circulation", correct: false},
+            { text: "Systemic Circulation", correct: true},
+        ]
+    },
+
+    {
+        question: "The organic molecule(s) present in the nucleus is/are",
+        answers: [
+            { text: "DNA", correct: true},
+            { text: "DNA and ATP", correct: false},
+            { text: "ATP and RNA", correct: false},
+            { text: "RNA", correct: false},
+        ]
+    },
+
+    {
+        question: "Albinism is an expression of trait controlled by",
+        answers: [
+            { text: "Chromosomes", correct: false},
+            { text: "Recessive genes", correct: true},
+            { text: "Dominant genes", correct: false},
+            { text: "Mutation", correct: false},
+        ]
+    },
+
+    {
+        question: "Which of the following cycles involve the process of precipitation and transpiration",
+        answers: [
+            { text: "Water cycles", correct: true},
+            { text: "Carbon cycle", correct: false},
+            { text: "Nitrogen cycle", correct: false},
+            { text: "Oxygen cycle", correct: false},
+        ]
+    },
+
+    {
+        question: "The simplest form of reproduction is",
+        answers: [
+            { text: "Conjugation", correct: false},
+            { text: "Budding", correct: false},
+            { text: "Spore formation", correct: true},
+            { text: "Binary fission", correct: false},
+        ]
+    },
+
+    {
+        question: "Suture joint is found in the",
+        answers: [
+            { text: "hip", correct: false},
+            { text: "ankle", correct: false},
+            { text: "skull", correct: false},
+            { text: "elbow", correct: true},
+        ]
+    },
+
+    {
+        question: "The piercing and sucking mouth parts are found in",
+        answers: [
+            { text: "grasshoppers", correct: false},
+            { text: "mosquitoes", correct: true},
+            { text: "termites", correct: false},
+            { text: "cockroaches", correct: false},
+        ]
+    },
+    {
+        question: "Which of the following is in organ level of organisation",
+        answers: [
+            { text: "Volvox sp", correct: false},
+            { text: "Paramecium caudatum", correct: false},
+            { text: "Hydra viridis", correct: false},
+            { text: "Onion bulb", correct: true},
+        ]
+    },
+
+    {
+        question: "The soil type with the least ability to retain nutrients is",
+        answers: [
+            { text: "sandy", correct: false},
+            { text: "clayey", correct: false},
+            { text: "loamy", correct: true},
+            { text: "clayey loam", correct: false},
+        ]
+    },
+    {
+        question: "The longest bone in the body is the",
+        answers: [
+            { text: "humerus", correct: false},
+            { text: "femur", correct: true},
+            { text: "scapula", correct: false},
+            { text: "tibia", correct: false},
+        ]
+    },
+];
+
+const questionElement = document.getElementById('question')
+const answerButtons = document.getElementById('answer-buttons')
+const nextButton = document.getElementById('next-btn')
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion(){
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener('click', selectAnswer);
+    });
+}
+
+function resetState(){
+    nextButton.style.display = 'none';
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild)
+    }
+}
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    } else{
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct")
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block"
+}
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = 'You scored ' + score + ' out of ' + questions.length + '!';
+    nextButton.innerHTML = "Play Again"
+    nextButton.style.display = 'block'
+}
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    } else{
+        showScore();
+    }
+}
+
+
+nextButton.addEventListener('click', ()=> {
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    } else{
+        startQuiz();
+    }
+})
+startQuiz();
+
+
